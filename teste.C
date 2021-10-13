9826c6146e538b692b45b36d964e1aff27dd1800
taisizidoro <62065331+taisizidoro@users.noreply.github.com>
2021-10-12 23:30:21 -0300

    Add files via upload

---
+++ teste.C
@@ -0,0 +1,25 @@
+
+void teste()
+{
+
+   RooRealVar x("x", "x", -100, 100);
+   RooRealVar cbmean("cbmean", "cbmean" , 0,  -9.0, 180.0) ;
+   RooRealVar cbsigma("cbsigma", "cbsigma" , 6.0, 1.0, 40.0) ;
+   RooRealVar cbsig("cbsig", "cbsignal", 10, 0, 1000000);
+   RooRealVar n("n","", 5.1);
+   RooRealVar alpha("alpha","", 1.3);
+
+   RooCrystalBall cball("cball", "crystal ball", x, cbmean, cbsigma, alpha, n);
+
+   RooDataSet* toyData = cball.generate(x,10000);
+
+   cball.fitTo(*toyData) ;
+
+
+   TCanvas* c1 = new TCanvas();
+   RooPlot* frame = x.frame() ;
+   toyData->plotOn(frame) ;
+   cball.plotOn(frame) ;;
+   frame->Draw() ;
+   c1->Draw() ;
+}
